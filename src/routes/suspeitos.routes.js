23bf6061e002274008ea15c9b96349447bf5fc47
Flavia Mendes <flavinharamendes@gmail.com>
2024-10-10 11:39:43 -0300

    feat: metodo post

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -40,7 +40,47 @@ let suspeitos = [
   },
 ];
 
-planetasRoutes.get("/", (req, res) => {
+suspeitosRoutes.get("/", (req, res) => {
     return res.status(200).json(suspeitos);
   });
+
+  suspeitosRoutes.post("/", (req,res) => {
+    const { nome, idade, envolvimento} = req.body
+
+    if (!nome || !idade || !envolvimento) {
+        return res.status(400).json({
+          message: "Os campos nome, idade e envolvimento são obrigatórios!",
+        });
+      }
+      if (envolvimento != "sim" && envolvimento != "não"){
+          return res.status(400).send({
+              message: "Coloque 'sim' ou 'não'!",
+          })
+      }
+      if ((Number.isInteger(idade)) == false) {
+          return res.status(400).send({
+              message: "A idade precisa ser um número inteiro."
+          })
+      }
+
+      const novoSuspeito = {
+        id: Math.floor(Math.random()*1000000),
+        nome,
+        idade,
+        envolvimento,
+      }
+      suspeitos.push(novoSuspeito)
+      return res.status(201).json({
+        message: "suspeito cadastrado com sucesso!",
+        novoSuspeito,
+      })
+  })
+
+
+
+
+
+
+
+
 export default suspeitosRoutes;
