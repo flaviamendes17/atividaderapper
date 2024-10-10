3fa8fc577112025a532fddd5ae8fc9524dc1c9c8
Flavia Mendes <flavinharamendes@gmail.com>
2024-10-10 10:33:55 -0300

    feat: criação do routes

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -3,11 +3,44 @@ import { Router } from "express";
 const suspeitosRoutes = Router();
 
 let suspeitos = [
-    {
+  {
     id: Math.floor(Math.random() * 1000000),
     nome: "Felipe Santos",
     idade: 32,
-    descricao: ["moreno", " tem barba", "usa óculos"],
+    descricao: ["moreno", "professor", "usa óculos"],
     envolvimento: true,
-    },
-]
+  },
+  {
+    id: Math.floor(Math.random() * 1000000),
+    nome: "P.Diddy",
+    idade: 54,
+    descricao: ["negro", "cantor", "cabelo castanho escuro"],
+    envolvimento: true,
+  },
+  {
+    id: Math.floor(Math.random() * 1000000),
+    nome: "Eminem",
+    idade: 51,
+    descricao: ["branco", "rapper", "tatuado"],
+    envolvimento: false,
+  },
+  {
+    id: Math.floor(Math.random() * 1000000),
+    nome: "Ashton Kutcher",
+    idade: 46,
+    descricao: ["branco", " ator", "cabelo castanho claro"],
+    envolvimento: true,
+  },
+  {
+    id: Math.floor(Math.random() * 1000000),
+    nome: "50 Cent",
+    idade: 49,
+    descricao: ["negro", "rapper", "tatuado"],
+    envolvimento: false,
+  },
+];
+
+planetasRoutes.get("/", (req, res) => {
+    return res.status(200).json(suspeitos);
+  });
+export default suspeitosRoutes;
