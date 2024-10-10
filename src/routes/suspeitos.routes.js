34511a6428cbc05267e932669614cf9f133e04c4
Flavia Mendes <flavinharamendes@gmail.com>
2024-10-10 11:58:47 -0300

    create: metodo put e delete

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -88,7 +88,60 @@ suspeitosRoutes.get("/", (req, res) => {
           return res.status(200).json(suspeito)
         })
 
-        
+        suspeitosRoutes.put("/:id", (req, res) => {
+            const { id } = req.params
+            const { nome, idade, envolvido, descricao} = req.body 
+
+            const suspeito = suspeitos.find((suspect) => suspect.id == id)
+
+            if (!nome || !idade || !envolvido) {
+                return res.status(400).json({
+                  message: "Os campos nome, idade, envolvido sao obrigatorios!",
+                })
+              }
+              if (envolvido != "sim" && envolvido != "não") {
+                return res.status(400).send({
+                  message: "Digite 'sim' ou 'não'! em envolvido",
+                })
+              }
+              if ((Number.isInteger(idade)) == false  ) {
+                return res.status(400).send({
+                  message: "Digite um numero inteiro para idade!!",
+                })
+              }
+            
+              suspeito.nome = nome
+              suspeito.idade = idade
+              suspeito.envolvido = envolvido
+              suspeito.descriçãoFisica = descricao
+            
+              return res.status(200).json({
+                message: "Suspeito atualizado com sucesso!",
+                suspeito,
+              })
+            })
+            
+            suspeitosRoutes.delete("/:id", (req, res) => {
+                const { id } = req.params
+              
+                
+                const suspeito = suspeitos.find((suspect) => suspect.id == id)
+              
+                
+                if (!suspeito) {
+                  return res
+                    .status(404)
+                    .json({ message: `suspeito com id ${id} não encontrado!` })
+              } 
+
+              suspeitos = suspeitos.filter((suspect) => suspect.id != id)
+
+return res.status(200).json({
+    message: "suspeito removido com sucesso!",
+    suspeito,
+  })
+})
+
 
 
 
