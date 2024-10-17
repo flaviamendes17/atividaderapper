ff8e1d249a0827c66c2a24cd5d3a0d2da4c5caf1
Flavia Mendes <flavinharamendes@gmail.com>
2024-10-17 14:37:52 -0300

    feat: atividade finalizada

--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -41,119 +41,109 @@ let suspeitos = [
 ];
 
 suspeitosRoutes.get("/", (req, res) => {
-    return res.status(200).json(suspeitos);
+  return res.status(200).json(suspeitos);
+});
+
+suspeitosRoutes.post("/", (req, res) => {
+  const { nome, idade, envolvimento } = req.body;
+
+  if (!nome || !idade || !envolvimento) {
+    return res.status(400).json({
+      message: "Os campos nome, idade e envolvimento são obrigatórios!",
+    });
+  }
+  if (envolvimento != "sim" && envolvimento != "não") {
+    return res.status(400).send({
+      message: "Coloque 'sim' ou 'não'!",
+    });
+  }
+  if (Number.isInteger(idade) == false) {
+    return res.status(400).send({
+      message: "A idade precisa ser um número inteiro.",
+    });
+  }
+
+  const novoSuspeito = {
+    id: Math.floor(Math.random() * 1000000),
+    nome,
+    idade,
+    envolvimento,
+  };
+  suspeitos.push(novoSuspeito);
+  return res.status(201).json({
+    message: "suspeito cadastrado com sucesso!",
+    novoSuspeito,
   });
+});
+suspeitosRoutes.get("/:id", (req, res) => {
+  const { id } = req.params;
 
-  suspeitosRoutes.post("/", (req,res) => {
-    const { nome, idade, envolvimento} = req.body
+  const suspeito = suspeitos.find((suspect) => suspect.id == id);
 
-    if (!nome || !idade || !envolvimento) {
-        return res.status(400).json({
-          message: "Os campos nome, idade e envolvimento são obrigatórios!",
-        });
-      }
-      if (envolvimento != "sim" && envolvimento != "não"){
-          return res.status(400).send({
-              message: "Coloque 'sim' ou 'não'!",
-          })
-      }
-      if ((Number.isInteger(idade)) == false) {
-          return res.status(400).send({
-              message: "A idade precisa ser um número inteiro."
-          })
-      }
+  if (!suspeito) {
+    return res
+      .status(404)
+      .json({ message: `suspeito com id ${id} não encontrado!` });
+  }
+  return res.status(200).json(suspeito);
+});
 
-      const novoSuspeito = {
-        id: Math.floor(Math.random()*1000000),
-        nome,
-        idade,
-        envolvimento,
-      }
-      suspeitos.push(novoSuspeito)
-      return res.status(201).json({
-        message: "suspeito cadastrado com sucesso!",
-        novoSuspeito,
-      })
-  })
-    suspeitosRoutes.get("/:id", (req, res) => {
-        const { id } = req.params
+suspeitosRoutes.put("/:id", (req, res) => {
+  const { id } = req.params;
+  const { nome, idade, envolvimento, descricao } = req.body;
+  const suspeito = suspeitos.find((suspect) => suspect.id == id);
 
-        const suspeito = suspeitos.find((suspect) => suspect.id == id)
-        
-        if (!suspeito) {
-            return res
-              .status(404)
-              .json({ message: `suspeito com id ${id} não encontrado!` })
-          }
-          return res.status(200).json(suspeito)
-        })
+  // Verifica se o suspeito foi encontrado
+  if (!suspeito) {
+    return res
+      .status(404)
+      .json({ message: `suspeito com id ${id} não encontrado!` });
+  }
 
-        suspeitosRoutes.put("/:id", (req, res) => {
-            const { id } = req.params
-            const { nome, idade, envolvimento, descricao} = req.body 
-            const suspeito = suspeitos.find((suspect) => suspect.id == id);
+  if (!nome || !idade || !envolvimento) {
+    return res.status(400).json({
+      message: "Os campos nome, idade, envolvimento sao obrigatorios!",
+    });
+  }
+  if (envolvimento != "sim" && envolvimento != "não") {
+    return res.status(400).send({
+      message: "Digite 'sim' ou 'não'! em envolvimento",
+    });
+  }
+  if (Number.isInteger(idade) == false) {
+    return res.status(400).send({
+      message: "Digite um numero inteiro para idade!!",
+    });
+  }
 
-            // Verifica se o suspeito foi encontrado
-            if (!suspeito) {
-              return res
-                .status(404)
-                .json({ message: `suspeito com id ${id} não encontrado!` });
-            }
-            
+  suspeito.nome = nome;
+  suspeito.idade = idade;
+  suspeito.envolvimento = envolvimento;
+  suspeito.descricao = descricao;
 
-            if (!nome || !idade || !envolvimento) {
-                return res.status(400).json({
-                  message: "Os campos nome, idade, envolvimento sao obrigatorios!",
-                })
-              }
-              if (envolvimento != "sim" && envolvimento != "não") {
-                return res.status(400).send({
-                  message: "Digite 'sim' ou 'não'! em envolvimento",
-                })
-              }
-              if ((Number.isInteger(idade)) == false  ) {
-                return res.status(400).send({
-                  message: "Digite um numero inteiro para idade!!",
-                })
-              }
-            
-              suspeito.nome = nome
-              suspeito.idade = idade
-              suspeito.envolvimento = envolvimento
-              suspeito.descricao = descricao
-            
-              return res.status(200).json({
-                message: "Suspeito atualizado com sucesso!",
-                suspeito,
-              })
-            })
-            
-            suspeitosRoutes.delete("/:id", (req, res) => {
-                const { id } = req.params
-              
-                
-                const suspeito = suspeitos.find((suspect) => suspect.id == id)
-              
-                
-                if (!suspeito) {
-                  return res
-                    .status(404)
-                    .json({ message: `suspeito com id ${id} não encontrado!` })
-              } 
+  return res.status(200).json({
+    message: "Suspeito atualizado com sucesso!",
+    suspeito,
+  });
+});
 
-              suspeitos = suspeitos.filter((suspect) => suspect.id != id)
+suspeitosRoutes.delete("/:id", (req, res) => {
+  const { id } = req.params;
 
-return res.status(200).json({
+  const suspeito = suspeitos.find((suspect) => suspect.id == id);
+
+  if (!suspeito) {
+    return res
+      .status(404)
+      .json({ message: `suspeito com id ${id} não encontrado!` });
+  }
+
+  suspeitos = suspeitos.filter((suspect) => suspect.id != id);
+
+  return res.status(200).json({
     message: "suspeito removido com sucesso!",
     suspeito,
-  })
-})
-
-
-
-
-
-
-
+  });
+});
 
 export default suspeitosRoutes;
